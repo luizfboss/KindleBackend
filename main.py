4e73fd49975d7e53005bbc66d315f6040ddc4891
luizfboss <88173271+luizfboss@users.noreply.github.com>
2023-05-17 01:51:05 -0300

    First File Created

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+PROMPT: Create a reading software, like Kindle, that shows some content on each page, 
+the user to move back and forth, add and remove books. 
+
+Split the content in each page, including title at the top of each page, and the page number at the bottom.
+
+------------------------------------------------------------------------
+All my reasoning and logic behind this project can be found in the "sketch.pdf" file in this repository.
+"""
